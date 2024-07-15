56e81db01519118f8f8fdb64331dad8f08d1dfba
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-15 12:01:45 -0700

    Add knight_moves.rb

---
+++ knight_moves.rb
@@ -0,0 +1,8 @@
+def knight_moves(start_point, end_point)
+  #check if start_point is valid
+  #check if end_point is valid
+  #return error message if not
+  #build tree for possible moves
+  #search tree keep count of moves
+  #return moves and message
+end
