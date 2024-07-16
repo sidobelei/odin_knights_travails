50970c0bc04c997a749874d472802f4c678360b0
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-16 15:40:35 -0700

    Add methods to create adjacency list
    The possible_moves method returns an array of
    valid moves for a specific position to the
    create_adjacency_list method.

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,10 +1,11 @@
 def knight_moves(start_point, end_point)
   #check if start_point is valid
   #check if end_point is valid
-  return nil unless valid?(start_point) && valid?(end_point)
   #return error message if not
-  #build tree for possible moves
-  #search tree keep count of moves
+  return nil unless valid?(start_point) && valid?(end_point)
+  #build adjacency list for possible moves
+  list = create_adjacency_list
+  #search list keep count of moves
   #return moves and message
   return true
 end
@@ -17,4 +18,26 @@ def valid?(coordinates)
     return false
   end
   return true
+end
+
+def create_adjacency_list
+  list = {}
+  positions = [0, 1, 2, 3, 4, 5, 6, 7]
+  positions.repeated_permutation(2) {|position| list[position] = []}
+  list.each {|key, value| list[key] = possible_moves(key)}
+  return list
+end
+
+def possible_moves(coordinates)
+  moves = []
+  shifts = [[-1, 2], [1, 2], [2, 1], [2, -1], [-1, -2], [1, -2], [-2, 1], [-2, -1]]
+  shifts.each do |shift| 
+    horizontal = shift[0] + coordinates[0]
+    vertical = shift[1] + coordinates[1]
+    move = [horizontal, vertical]
+    if valid?(move)
+      moves.push(move) 
+    end
+  end 
+  return moves
 end
