f1cb28dcd7a520b42cbe1d6fd2fc3887590401f5
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-07-16 13:47:46 -0700

    Add valid? method
    This method checks if the coordinates of the
    knights's position is valid.

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,8 +1,20 @@
 def knight_moves(start_point, end_point)
   #check if start_point is valid
   #check if end_point is valid
+  return nil unless valid?(start_point) && valid?(end_point)
   #return error message if not
   #build tree for possible moves
   #search tree keep count of moves
   #return moves and message
+  return true
+end
+
+def valid?(coordinates)
+  if coordinates[0] < 0 || coordinates[0] > 7
+    return false
+  end
+  if coordinates[1] < 0 || coordinates[1] > 7
+    return false
+  end
+  return true
 end
